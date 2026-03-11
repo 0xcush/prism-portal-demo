56237da35824a6a161940530ff2e24bc558b1bd8
cush.eth <cush.eth@MacBook-Pro-150.local>
2026-03-11 12:31:07 -0600

    Fix: switch to @astrojs/vercel adapter for Vercel deploy

--- dist/server/chunks/request-context_NulmSr9Y.mjs
+++
@@ -1,8 +0,0 @@
-import { AsyncLocalStorage } from 'node:async_hooks';
-
-const requestContext = new AsyncLocalStorage();
-function getRequestToken() {
-  return requestContext.getStore()?.token;
-}
-
-export { getRequestToken as g, requestContext as r };
